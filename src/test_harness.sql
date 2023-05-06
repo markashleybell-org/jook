f03f07b459e817981ded77ee0243133af36a25e7
Mark Bell <me@markb.co.uk>
2023-05-06 12:29:28 +0100

    Test harness updates

--- src/test_harness.sql
+++ src/test_harness.sql
@@ -2,11 +2,32 @@
 -- SELECT * FROM Albums
 -- SELECT * FROM Tracks
 
+
 SELECT 
     ar.Name,
     ab.Title,
     t.Title,
     t.Url
+FROM 
+    Tracks t
+INNER JOIN
+    Albums ab ON ab.AlbumID = t.AlbumID
+INNER JOIN
+    Artists ar ON ar.ArtistID = ab.ArtistID
+ORDER BY
+    ar.Name,
+    ab.Title,
+    t.Title
+
+
+SELECT 
+    ar.ArtistID,
+    ar.Name AS ArtistName,
+    ab.AlbumID,
+    ab.Title AS AlbumTitle,
+    t.TrackID,
+    t.Title AS TrackTitle,
+    t.Url AS Url
 FROM 
     Tracks t
 INNER JOIN
