2f921e7332d5c049dae94eeabad7798f7c03b389
Mark Bell <me@markb.co.uk>
2023-10-10 20:20:39 +0100

    Use minified static assets

--- src/app/wwwroot/js/src/main.js
+++ src/app/wwwroot/js/src/main.js
@@ -1,6 +1,6 @@
 import mustache from "https://cdnjs.cloudflare.com/ajax/libs/mustache.js/4.2.0/mustache.min.js";
 
-import { config } from "./config.js"
+import { config } from "./config.min.js"
 
 const player = document.querySelector("audio");
 
