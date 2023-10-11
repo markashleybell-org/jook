6e8f83daa22f882636f2b1b4bcf26e919772a1ec
Mark Bell <me@markb.co.uk>
2023-10-11 09:12:38 +0100

    Fix broken source map
    I don't know what this flag does, and there is no documentation I can find, but it fixes the problem, and I can't be bothered to waste yet more hours of my life trying to figure out broken NPM-based build tooling.
    
    The cleancss package is in maintenance mode anyway so I'll have to find a replacement at some point.

--- src/app/build-front-end.ps1
+++ src/app/build-front-end.ps1
@@ -6,7 +6,7 @@ New-Item ./wwwroot/js/dist -ItemType Directory -ErrorAction SilentlyContinue | O
 # CSS Build
 Copy-Item ./node_modules/bootstrap/dist/css/bootstrap.min.css ./wwwroot/css/dist
 Copy-Item ./node_modules/bootstrap/dist/css/bootstrap.min.css.map ./wwwroot/css/dist
-npx cleancss --source-map --output ./wwwroot/css/dist/main.min.css ./wwwroot/css/src/main.css
+npx cleancss --source-map-inline-sources --output ./wwwroot/css/dist/main.min.css ./wwwroot/css/src/main.css
 
 # JS Build
 npx terser ./wwwroot/js/src/main.js --output ./wwwroot/js/dist/main.min.js --source-map --module --ecma 2020
--- src/app/build-front-end.sh
+++ src/app/build-front-end.sh
@@ -5,7 +5,7 @@ mkdir -p ./wwwroot/js/dist
 # CSS Build
 cp ./node_modules/bootstrap/dist/css/bootstrap.min.css ./wwwroot/css/dist
 cp ./node_modules/bootstrap/dist/css/bootstrap.min.css.map ./wwwroot/css/dist
-npx cleancss --source-map --output ./wwwroot/css/dist/main.min.css ./wwwroot/css/src/main.css
+npx cleancss --source-map-inline-sources --output ./wwwroot/css/dist/main.min.css ./wwwroot/css/src/main.css
 
 # JS Build
 npx terser ./wwwroot/js/src/main.js --output ./wwwroot/js/dist/main.min.js --source-map --module --ecma 2020
